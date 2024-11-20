f43f157a8d0a7568c37d6c859f931bdcccb26f9e
HottyMcShotty <samueliscool2015@gmail.com>
2024-11-20 11:49:16 -0600

    Simplified Code, removed tkinter and for loops

--- main.py
+++ main.py
@@ -1,61 +1,18 @@
-import tkinter as tk
 import requests
 api_key="RGAPI-0a55a7f4-92b0-4792-8b77-456820b5f6aa"
-text="The%20Troglodyte"
+name="The%20Troglodyte"
 id="1111"
-api_url="https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/"+text+"/"+id+"?api_key="+api_key
+api_url="https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/"+name+"/"+id+"?api_key="+api_key
 
 #getting the json file, then parsing the json file, and then grabbing the puuid
-account=requests.get(api_url)
-account=account.json()
-puuid=account["puuid"]
+puuid=requests.get(api_url).json()["puuid"]
+
 matches_url="https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/"+puuid+"/ids?start=0&count=10&api_key="+api_key
-matches_requests=requests.get(matches_url)
-matches=matches_requests.json()
-kdalist=[None]*10
-for x in range(10):
-    match_id=matches[0]
-    temp_match="https://americas.api.riotgames.com/lol/match/v5/matches/"+match_id+"?api_key="+api_key
-    match_data_call=requests.get(temp_match)
-    match_data=match_data_call.json()
-    info=match_data['metadata']["participants"]
-    temp_puuid=info[x]
-    for y in range(5):
-        temp_matches_url="https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/"+temp_puuid+"/ids?start=0&count=5&api_key="+api_key
-        temp_matches_requests=requests.get(temp_matches_url)
-        temp_matches=temp_matches_requests.json()
-        temp_match_id=temp_matches[y]
-        temp_match_data_call=requests.get(temp_match)
-        temp_match_data=match_data_call.json()
-        kda=temp_match_data["info"]["participants"][info]['challenges']["kda"]
-        
-for x in range(10):
-    print(kdalist[x])
-        
-
-    #kda=match_data["info"]["participants"][info]['challenges']["kda"]
-print(match_id)
+matches=requests.get(matches_url).json()[0]
 
 
-'''
-def update_label(x):
-    if x[0] % 2 == 0:
-        label.config(text=kda)
-    else:
-        label.config(text="Original Text")
-    x[0] += 1
 
-root = tk.Tk()
-root.title("Tkinter Update Example")
-root.geometry("300x300")
-
-x = [0]  # Use a list to hold the mutable value
-
-label = tk.Label(root, text="Original Text")
-label.grid(row=0, column=0, padx=10, pady=10)
-
-button = tk.Button(root, text="Update Text!", command=lambda: update_label(x))
-button.grid(row=1, column=1, padx=10, pady=10)
-
-root.mainloop()
-'''
+temp_match="https://americas.api.riotgames.com/lol/match/v5/matches/"+matches+"?api_key="+api_key
+match_data_call=requests.get(temp_match).json()
+info=match_data_call['metadata']["participants"]
+print(matches)
