e2cb99cd3278d03661d72f2fd7075f24cbd2d181
HottyMcShotty <samueliscool2015@gmail.com>
2024-11-20 17:53:13 -0600

    Info Obtained now gets boiled down and saved into a dictionary

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import requests
 import time
-api_key="RGAPI-0a55a7f4-92b0-4792-8b77-456820b5f6aa"
+API_KEY="RGAPI-0a55a7f4-92b0-4792-8b77-456820b5f6aa"
 name="The%20Troglodyte"
 id="1111"
-api_url="https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/"+name+"/"+id+"?api_key="+api_key
+api_url="https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/"+name+"/"+id+"?api_key="+API_KEY
 kda =0
 playerList=[None]*10
 
 
 def obtain():
     totalkda=0
+    playerInfo=[]
     #getting the json file, then parsing the json file, and then grabbing the puuid
     puuid=requests.get(api_url).json()["puuid"]
 
-    matches_url="https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/"+puuid+"/ids?start=0&count=5&api_key="+api_key
+    matches_url="https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/"+puuid+"/ids?start=0&count=5&api_key="+API_KEY
     lastMatch=requests.get(matches_url).json()[0]
-    lastMatchurl="https://americas.api.riotgames.com/lol/match/v5/matches/"+lastMatch+"?api_key="+api_key
+    lastMatchurl="https://americas.api.riotgames.com/lol/match/v5/matches/"+lastMatch+"?api_key="+API_KEY
 
     #putting all 10 players from current/lastmatch into a list
     for x in range(10):
@@ -27,24 +28,28 @@ def obtain():
     #player list while we iterate through the games then repeat for all players I would note that the best way to
     #store this info will most likely be a dictionary inside of a list.
     for x in range(10):
+        
         ####Iteating through our player list and getting match ids for their last 10 matches
-        matches_url="https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/"+playerList[x]+"/ids?start=0&count=5&api_key="+api_key
+        matches_url="https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/"+playerList[x]+"/ids?start=0&count=5&api_key="+API_KEY
         for y in range(5):
             try:
                 matches=requests.get(matches_url).json()[y]
                 ####
                 ####Making a data call for the first player in game and the match that is equal to index x
-                temp_match="https://americas.api.riotgames.com/lol/match/v5/matches/"+matches+"?api_key="+api_key
+                temp_match="https://americas.api.riotgames.com/lol/match/v5/matches/"+matches+"?api_key="+API_KEY
                 match_data_call=requests.get(temp_match).json()
                 ####Grabbing Data From the Match
                 info=match_data_call['metadata']["participants"].index(playerList[x])
                 kda =match_data_call["info"]["participants"][info]["challenges"]["kda"]
                 temp_name=match_data_call["info"]["participants"][info]["summonerName"]
                 time.sleep(1.5)
-                print(temp_name,kda)
                 totalkda=totalkda+kda
-            except:
-                print("Well that just happened!")
+                
+            except Exception as e:
+                print(f"An error occurred: {e}")
         print("Average KDA:"+str(totalkda/5))
+        playerInfo.append({"Name":temp_name,"KDA":totalkda/5})
+        totalkda=0
         ####
+    print(playerInfo)
 obtain()
