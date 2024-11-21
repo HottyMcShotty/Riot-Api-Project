920941cb9df94ea9ef917b0359eab537903a72b8
HottyMcShotty <samueliscool2015@gmail.com>
2024-11-21 00:16:09 -0600

    Added Winrate Tracker

--- main.py
+++ main.py
@@ -30,10 +30,10 @@ def obtain():
     #player list while we iterate through the games then repeat for all players I would note that the best way to
     #store this info will most likely be a dictionary inside of a list.
     for x in range(10):
-        
+        winsum=0
         ####Iteating through our player list and getting match ids for their last 10 matches
         matches_url="https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/"+playerList[x]+"/ids?start=0&count=5&api_key="+API_KEY
-        for y in range(1):
+        for y in range(5):
             try:
                 matches=requests.get(matches_url).json()[y]
                 ####
@@ -44,14 +44,20 @@ def obtain():
                 info=match_data_call['metadata']["participants"].index(playerList[x])
                 kda =match_data_call["info"]["participants"][info]["challenges"]["kda"]
                 time.sleep(1.5)
+                win =match_data_call["info"]["participants"][info]["win"]
                 totalkda=totalkda+kda
+                winsum+=int(win)
+                
+                
                 
             except Exception as e:
                 print(f"An error occurred: {e}")
+        winsum/=5
         print(str((1+x)*10)+"% Done!")
         temp_name=match_data_call["info"]["participants"][info]["summonerName"]
-        playerInfo.append({"Name":temp_name,"KDA":round((totalkda/5),2)})
+        playerInfo.append({"Name":temp_name,"KDA":round((totalkda/5),2),"Win":str(winsum*100)})
         totalkda=0
+        win=0
         ####
 
 
@@ -68,6 +74,9 @@ def gui():
     header_kda = tk.Label(root, text="KDA", font=("Arial", 12, "bold"))
     header_kda.grid(row=0, column=1, padx=10, pady=5)
 
+    win_kda = tk.Label(root, text="Win", font=("Arial", 12, "bold"))
+    win_kda.grid(row=0, column=2, padx=10, pady=5)
+
     # Populate the table with data
     for i in range(10):
         name_label = tk.Label(root, text=playerInfo[i]["Name"],fg=("red"), font=("Arial", 10))
@@ -75,6 +84,9 @@ def gui():
 
         kda_label = tk.Label(root, text=str(playerInfo[i]["KDA"]), font=("Arial", 10))
         kda_label.grid(row=i + 1, column=1, padx=10, pady=5)
+
+        win_label = tk.Label(root, text=str(playerInfo[i]["Win"]), font=("Arial", 10))
+        win_label.grid(row=i + 1, column=2, padx=10, pady=5)
 
     # Run the application
     root.mainloop()
