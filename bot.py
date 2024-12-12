bd0dbe7a7134786e5e939d6074a349f07de8f68d
HottyMcShotty <samueliscool2015@gmail.com>
2024-12-12 14:01:32 -0600

    Changes Made

--- bot.py
+++ bot.py
@@ -2,18 +2,16 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+import main
+from main import obtain
 
 # Load the .env file
 load_dotenv(dotenv_path="C:/Users/samue/OneDrive/Desktop/LeagueCompanion/Riot-Api-Project/.env")
 BOT_KEY = os.getenv("BOT_KEY")
-print(f"Loaded BOT_KEY: {BOT_KEY}")  # Debugging line
+print(f"Loaded BOT_KEY: {BOT_KEY}")  # Debugging line``
 
-if BOT_KEY is None:
-    raise ValueError("BOT_KEY is not set. Check your .env file.")
-
-# Intents setup
 intents = discord.Intents.default()
-intents.messages = True
+intents.message_content = True
 
 # Bot prefix and initialization
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -24,16 +22,6 @@ async def on_ready():
 
 @bot.command()
 async def ping(ctx):
-    latency = round(bot.latency * 1000)
-    await ctx.send(f"Pong! Latency: {latency}ms")
-
-@bot.command()
-async def say(ctx, *, message: str):
-    await ctx.send(message)
-
-@bot.command()
-async def add(ctx, a: int, b: int):
-    result = a + b
-    await ctx.send(f"The sum of {a} and {b} is {result}.")
+    await ctx.send(obtain())
 
 bot.run(BOT_KEY)
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import discord
+from discord.ext import commands
 import requests
 import time
 import tkinter as tk
@@ -11,6 +13,9 @@ id="1111"
 api_url="https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/"+name+"/"+id+"?api_key="+API_KEY
 kda =0
 playerList=[None]*10
+load_dotenv(dotenv_path="C:/Users/samue/OneDrive/Desktop/LeagueCompanion/Riot-Api-Project/.env")
+BOT_KEY = os.getenv("BOT_KEY")
+print(f"Loaded BOT_KEY: {BOT_KEY}")  # Debugging line``
 
 
 def obtain():
@@ -62,7 +67,9 @@ def obtain():
         playerInfo.append({"Name":temp_name,"KDA":round((totalkda/5),2),"Win":str(winsum*100)})
         totalkda=0
         win=0
+        
         ####
+    return playerInfo
 
 
 def gui():
@@ -94,4 +101,21 @@ def gui():
 
     # Run the application
     root.mainloop()
-gui()
+
+def botty():
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    # Bot prefix and initialization
+    bot = commands.Bot(command_prefix="!", intents=intents)
+
+    @bot.event
+    async def on_ready():
+        print(f"Bot is online as {bot.user}")
+
+    @bot.command()
+    async def ping(ctx):
+        await ctx.send(obtain())
+
+    bot.run(BOT_KEY)
+botty()
