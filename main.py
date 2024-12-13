3d0cb9a7a7548beeb9594dbce2d214ad6b1c69cf
HottyMcShotty <samueliscool2015@gmail.com>
2024-12-13 15:09:40 -0600

    Bot sends stats as they are obtained now

--- main.py
+++ main.py
@@ -18,9 +18,8 @@ BOT_KEY = os.getenv("BOT_KEY")
 print(f"Loaded BOT_KEY: {BOT_KEY}")  # Debugging line``
 
 
-def obtain():
+async def obtain(ctx):
     totalkda=0
-    global playerInfo
     playerInfo=[]
     #getting the json file, then parsing the json file, and then grabbing the puuid
     puuid=requests.get(api_url).json()["puuid"]
@@ -64,42 +63,16 @@ def obtain():
         print(str((1+x)*10)+"% Done!")
         temp_name=match_data_call["info"]["participants"][info]["summonerName"]
         playerInfo.append({"Name":temp_name,"KDA":round((totalkda/5),2),"Win":str(winsum*100)})
+        await ctx.send(f"Player stats completed:\tName: {temp_name}\tKDA: {round((totalkda/3),2)}\tWin Rate: {round((winsum * 100),2)}%")
         totalkda=0
         win=0
+        
         
         ####
     return playerInfo
 
 
-def gui():
-    obtain()
-    root = tk.Tk()
-    root.title("Player Stats")
-    root.attributes("-topmost", True)
 
-    # Create a header for the table
-    header_name = tk.Label(root, text="Name", font=("Arial", 12, "bold"))
-    header_name.grid(row=0, column=0, padx=10, pady=5)
-
-    header_kda = tk.Label(root, text="KDA", font=("Arial", 12, "bold"))
-    header_kda.grid(row=0, column=1, padx=10, pady=5)
-
-    win_kda = tk.Label(root, text="Win", font=("Arial", 12, "bold"))
-    win_kda.grid(row=0, column=2, padx=10, pady=5)
-
-    # Populate the table with data
-    for i in range(10):
-        name_label = tk.Label(root, text=playerInfo[i]["Name"],fg=("red"), font=("Arial", 10))
-        name_label.grid(row=i + 1, column=0, padx=10, pady=5)
-
-        kda_label = tk.Label(root, text=str(playerInfo[i]["KDA"]), font=("Arial", 10))
-        kda_label.grid(row=i + 1, column=1, padx=10, pady=5)
-
-        win_label = tk.Label(root, text=str(playerInfo[i]["Win"]), font=("Arial", 10))
-        win_label.grid(row=i + 1, column=2, padx=10, pady=5)
-
-    # Run the application
-    root.mainloop()
 
 def botty():
     intents = discord.Intents.default()
@@ -114,9 +87,8 @@ def botty():
 
     @bot.command()
     async def ping(ctx):
-        obtain()
-        for x in range(10):
-            await ctx.send(playerInfo[x])
+        await ctx.send("Gathering Data, this may take a moment")
+        await obtain(ctx)
 
     bot.run(BOT_KEY)
 botty()
